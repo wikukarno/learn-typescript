f5e18956c3c5057f28281202b9cf4476392d4100
wikukarno <prasetyagama2@gmail.com>
2024-01-18 14:40:10 +0700

    Learn Array (Typescript)

--- dist/tests/tipe-data.test.js
+++ dist/tests/tipe-data.test.js
@@ -1,14 +1,11 @@
 import { info } from "console";
 describe('Data Type', function () {
     it('should must declare', function () {
-        let name = "Wiku karno Prasetyagama";
-        let balance = 1000000;
-        let isVip = true;
+        const name = "Wiku karno Prasetyagama";
+        const balance = 1000000;
+        const isVip = true;
         info(`Name: ${name}`);
         info(`Balance: ${balance}`);
         info(`Is VIP: ${isVip}`);
-        name = 100;
-        balance = "1000000";
-        isVip = "true";
     });
 });
